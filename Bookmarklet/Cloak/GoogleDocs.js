2a2ff1ac06293392a835d59a779196b911b27b5d
Google <58515773+TheMysteryperson@users.noreply.github.com>
2021-05-20 09:44:59 -0400

    Create GoogleDocs.js

---
+++ Bookmarklet/Cloak/GoogleDocs.js
@@ -0,0 +1,1 @@
+javascript:(function() { setInterval(function() { var link = document.querySelector('link[rel*="icon"]') || document.createElement('link'); link.type = 'image/x-icon'; link.rel = 'shortcut icon'; link.href = 'https://ssl.gstatic.com/docs/documents/images/kix-favicon7.ico'; document.getElementsByTagName('head')[0].appendChild(link); document.title = 'Google Docs';}, 1); })();
